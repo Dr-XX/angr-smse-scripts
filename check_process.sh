3ecabf1941643ebb471c2d05c6d98276e95ccc75
Dr-XX <jordan0801@163.com>
2019-09-23 16:45:52 +0800

    add monitor script

---
+++ check_process.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+dir_array=(
+"/home/jordan/tests/jpeg-9c/install/bin/angr-result-depth"
+"/home/jordan/tests/libav/install/bin/angr-result-depth"
+"/home/jordan/tests/libav/install/bin/angr-result-depth"
+"/home/jordan/tests/FreeImage/Examples/Generic/angr-result-depth"
+)
+
+script_array=(
+"angr_drive.py"
+"drive.py"
+"angr_drive.py"
+"drive.py"
+"angr_drive.py"
+)
+
+i=0
+while true; do
+    sleep 10
+    echo "sleep"
+    count=`ps -ef|grep jordan|grep python3|wc -l`
+    if [ $count -eq 0 ]
+    then
+        #echo $count
+        echo ${dir_array[$i]},${script_array[$i]}
+        cd ${dir_array[$i]}
+        #head ../${script_array[$i]}
+        timeout -s INT 6h python3 ../${script_array[$i]} 
+        i=$i+1
+    fi
+    if [ $i -eq 5 ]
+    then
+        break
+    fi
+done
