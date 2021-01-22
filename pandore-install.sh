8598526e941288af3e48917fd379862b91e8fd98
Dr-XX <jordan0801@163.com>
2021-01-22 17:06:50 +0800

    remove duplicated files

--- pandore-install.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-#first, modify "configure"
-#CXX=${CXX}
-#CXXFLAGS="$CFLAGS -DNDEBUG -O0 -Iinclude"
-
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-CC=gcc CXX=g++ ./configure --prefix=/home/jordan/tests/pandore/install
-#CC=gclang CXX=gclang++
--- run_klee_imagemagick.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-klee -write-cvcs -write-cov -output-module -max-memory=63000 -max-time=21600 -watchdog -only-output-states-covering-new -debug-crosscheck-core-solver=z3 -solver-backend=z3 -search=dfs -link-llvm-lib=/home/jordan/tests/ImageMagick/install-support/lib/libMagickCore-7.Q16.bca -link-llvm-lib=/home/jordan/tests/ImageMagick/install-support/lib/libMagickWand-7.Q16.bca -link-llvm-lib=/home/jordan/tests/ImageMagick/install-support/lib/libMagick++-7.Q16.bca -libc=uclibc -posix-runtime /home/jordan/tests/ImageMagick/install-support/bin/magick.bc identify -verbose A -sym-files 1 500
